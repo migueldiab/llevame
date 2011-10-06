8cc6900eb2b1b026600239b781b65dbd972d7db8
unknown <migueldiab@.(none)>
2011-10-05 22:15:02 -0200

    inicio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LlevameCom::Application.config.secret_token = '54f8c92f35a88976e1d1db5d1cd4c7a3b8ba6e9fc6840261b9bd65ad766a5aae0919c3dd866defd71894eac1c7004859be3e61d316bd4154709e3396eb0a3615'
