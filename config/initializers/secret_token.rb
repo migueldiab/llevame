7075ca5225cc777a31f6955724c9479368def6fe
unknown <migueldiab@.(none)>
2011-10-16 09:37:30 -0200

    Tabula Rasa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Llevame::Application.config.secret_token = '3a9e56606b381a67e732845c64725750e4d9ede5246803c88878980d11152358554f6627daa426b01b8f9fe50fc2caef3655644648257fa15536d108310ba09b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Llevame::Application.config.session_store :cookie_store, key: '_llevame_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Llevame::Application.config.session_store :active_record_store
