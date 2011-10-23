5b52455a66a47c397bcca4abd4f39db0b93f0213
unknown <migueldiab@.(none)>
2011-10-23 20:25:06 -0200

    Tabula Rasa 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Llevame::Application.config.secret_token = '3953a7580c67b6c4f8103cf970029f3c954ca7e75cc304a2e78d703f3d8f1ecce3d0c82438ec60884295e8232f76ba0fe37212e01a362397651115f48dedb8a0'
