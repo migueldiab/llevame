570c6a6fc44a02213d4168a334143fc9ac25f67c
Miguel A. Diab <migueldiab@adinet.com.uy>
2012-10-21 23:27:13 -0200

    ADD Lazy Load de Imagenes en Landing

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -7,10 +7,16 @@ function startSlider() {
    $('#button a').click(function(){
     var integer = $(this).attr('rel');
     $('#myslide .cover').animate({left:-320*(parseInt(integer)-1)})  /*----- Width of div mystuff (here 160) ------ */
-    $('#button a').each(function(){
-    $(this).removeClass('active');
+    $('#button a').each(function() {
+      $(this).removeClass('active');
       if($(this).hasClass('button'+integer)){
-        $(this).addClass('active')}
+        $(this).addClass('active')
+      }
     });
+    $('img.lazy').lazyload({
+      effect: "fadeIn"
+    }); 
   }); 
 }
+
+
