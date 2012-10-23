201b6de0616ee6ae24a48446bf9ef58f8aece3ae
Miguel A. Diab <migueldiab@adinet.com.uy>
2012-10-22 23:15:31 -0200

    Modal para Registro

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,12 @@
 $(document).ready(function (){
   startSlider();
+  
+  $('img.lazy').each(function() {
+    this.hidden = true;
+  });
+
+  // $('#btnRegister').click(showRegisterForm);
+
 });
 
 
@@ -13,10 +20,22 @@ function startSlider() {
         $(this).addClass('active')
       }
     });
-    $('img.lazy').lazyload({
-      effect: "fadeIn"
-    }); 
+
+    if ($('img.lazy')[0].hidden) {
+      $('img.lazy').each(function() {
+        this.hidden = false;
+      });
+      $('img.lazy').lazyload({
+        effect : "fadeIn"
+      }); 
+    }
   }); 
 }
+
+function showRegisterForm() {
+  //$.modal("<div id='osx-container'><h1>SimpleModal</h1></div>");
+  return false;
+}
+
 
 
